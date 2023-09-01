4a46ea40f77e5e0594c41b139dd1bb44695e23d4
Fenitriniaina <git.fandrianarisata@gmail.com>
2023-09-01 12:43:22 +0300

    add image home page + SEO

--- backend/src/Controller/Admin/HomeCrudController.php
+++ backend/src/Controller/Admin/HomeCrudController.php
@@ -3,12 +3,15 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Home;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 
 class HomeCrudController extends AbstractCrudController
 {
@@ -20,8 +23,16 @@ class HomeCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('title'),
+            TextField::new('titleH1'),
             TextEditorField::new('details'),
+            TextField::new('imageFile')->setFormType(VichImageType::class)->onlyOnForms(),
+            ImageField::new('image')
+                ->setBasePath('/uploads/images/home/')
+                ->setUploadDir('public/uploads/images/home')->hideOnForm(),
+            TextField::new('metaTitle'),
+            TextField::new('metaKey'),
+            TextareaField::new('metaDescription')
+            
         ];
     }
     public function configureActions(Actions $actions): Actions
--- backend/src/Entity/Home.php
+++ backend/src/Entity/Home.php
@@ -2,11 +2,14 @@
 
 namespace App\Entity;
 
-use ApiPlatform\Metadata\ApiResource;
-use App\Repository\HomeRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\HomeRepository;
+use ApiPlatform\Metadata\ApiResource;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: HomeRepository::class)]
 #[ApiResource]
 class Home
@@ -19,15 +22,50 @@ class Home
     #[ORM\Column(length: 255)]
     private ?string $titleH1 = null;
 
+    #[Vich\UploadableField(mapping: 'home_images', fileNameProperty: 'image')]
+    private ?File $imageFile = null;
+
+
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $image = null;
 
     #[ORM\Column(type: Types::TEXT)]
     private ?string $details = null;
 
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $createdAt = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $metaKey = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $metaDescription = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $metaTitle = null;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
     }
 
     public function getTitleH1(): ?string
@@ -38,6 +76,29 @@ class Home
     public function setTitleH1(string $titleH1): static
     {
         $this->titleH1 = $titleH1;
+
+        return $this;
+    }
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): static
+    {
+        $this->updatedAt = $updatedAt;
 
         return $this;
     }
@@ -62,6 +123,42 @@ class Home
     public function setDetails(string $details): static
     {
         $this->details = $details;
+
+        return $this;
+    }
+
+    public function getMetaKey(): ?string
+    {
+        return $this->metaKey;
+    }
+
+    public function setMetaKey(?string $metaKey): static
+    {
+        $this->metaKey = $metaKey;
+
+        return $this;
+    }
+
+    public function getMetaDescription(): ?string
+    {
+        return $this->metaDescription;
+    }
+
+    public function setMetaDescription(?string $metaDescription): static
+    {
+        $this->metaDescription = $metaDescription;
+
+        return $this;
+    }
+
+    public function getMetaTitle(): ?string
+    {
+        return $this->metaTitle;
+    }
+
+    public function setMetaTitle(?string $metaTitle): static
+    {
+        $this->metaTitle = $metaTitle;
 
         return $this;
     }
